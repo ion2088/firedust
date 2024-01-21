68184ca6055255dddc62dce7f6d6abfb65bcb6f7
Ion <iontapordei@gmail.com>
2024-01-21 19:31:53 +0000

    add async support to the API client

--- src/firedust/utilities/api.py
+++ src/firedust/utilities/api.py
@@ -1,4 +1,4 @@
-import requests
+import httpx
 import os
 
 from firedust.utilities.errors import MissingFiredustKeyError
@@ -40,19 +40,39 @@ class APIClient:
             "Authorization": f"Bearer {api_key}",
         }
 
+    # sync methods
     def get(self, url: str, params: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
-        return self._request("get", url, params=params)
+        return self._request_sync("get", url, params=params)
 
     def post(self, url: str, data: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
-        return self._request("post", url, data=data)
+        return self._request_sync("post", url, data=data)
 
     def put(self, url: str, data: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
-        return self._request("put", url, data=data)
+        return self._request_sync("put", url, data=data)
 
     def delete(self, url: str) -> Dict[str, Any]:
-        return self._request("delete", url)
+        return self._request_sync("delete", url)
 
-    def _request(
+    # async methods
+    async def get_async(
+        self, url: str, params: Optional[Dict[str, Any]] = None
+    ) -> Dict[str, Any]:
+        return await self._request_async("get", url, params=params)
+
+    async def post_async(
+        self, url: str, data: Optional[Dict[str, Any]] = None
+    ) -> Dict[str, Any]:
+        return await self._request_async("post", url, data=data)
+
+    async def put_async(
+        self, url: str, data: Optional[Dict[str, Any]] = None
+    ) -> Dict[str, Any]:
+        return await self._request_async("put", url, data=data)
+
+    async def delete_async(self, url: str) -> Dict[str, Any]:
+        return await self._request_async("delete", url)
+
+    def _request_sync(
         self,
         method: str,
         url: str,
@@ -60,12 +80,45 @@ class APIClient:
         data: Optional[Dict[str, Any]] = None,
     ) -> Dict[str, Any] | Any:
         url = self.base_url + url
-        response = requests.request(
+        response = httpx.request(
             method, url, params=params, json=data, headers=self.headers
         )
         response.raise_for_status()
 
-        if response.status_code > 299:
-            raise APIError(response.text, response.status_code)
+        # Process status codes and raise errors
+        _process_status_codes(response)
 
         return response.json()
+
+    async def _request_async(
+        self,
+        method: str,
+        url: str,
+        params: Optional[Dict[str, Any]] = None,
+        data: Optional[Dict[str, Any]] = None,
+    ) -> Dict[str, Any] | Any:
+        url = self.base_url + url
+        async with httpx.AsyncClient() as client:
+            response = await client.request(
+                method, url, params=params, json=data, headers=self.headers
+            )
+            response.raise_for_status()
+
+            # Process status codes and raise errors
+            _process_status_codes(response)
+
+            return response.json()
+
+
+def _process_status_codes(response: httpx.Response) -> None:
+    if response.status_code == 400:
+        raise APIError("Bad Request", response.status_code)
+    elif response.status_code == 401:
+        raise APIError("Unauthorized", response.status_code)
+    elif response.status_code == 403:
+        raise APIError("Forbidden", response.status_code)
+    elif response.status_code == 404:
+        raise APIError("Not Found", response.status_code)
+    elif response.status_code == 500:
+        raise APIError("Internal Server Error", response.status_code)
+    # TODO: Customize status codes and error messages
