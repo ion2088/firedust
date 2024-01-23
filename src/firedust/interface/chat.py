abe21d3afdb222572a28d7c183ab6fdfdac87ea4
Ion <iontapordei@gmail.com>
2024-01-23 18:24:09 +0000

    add a user_id parameter to assistant chats

--- src/firedust/interface/chat.py
+++ src/firedust/interface/chat.py
@@ -1,18 +1,27 @@
+"""
+Chat interface module for the Firedust assistant.
+
+Example:
+    import firedust
+
+    assistant = firedust.assistant.load("ASSISTANT_ID")
+
+    # Chat with the assistant
+    response = assistant.chat.stream("Tell me about the Book of the Dead")
+
+    for msg in response:
+        print(msg)
+"""
+
+from typing import Iterator
+from uuid import UUID
 from firedust._utils.api import APIClient
 from firedust._utils.types.assistant import AssistantConfig
-from typing import Iterator
 
 
 class Chat:
     """
     A collection of methods to chat with the assistant.
-
-    To chat with the assistant, you can use the following methods:
-
-        response = assistant.chat.stream("Hello")
-
-        for msg in response:
-            print(msg)
     """
 
     def __init__(self, config: AssistantConfig, api_client: APIClient) -> None:
@@ -26,12 +35,18 @@ class Chat:
         self.config = config
         self.api_client = api_client
 
-    def stream(self, message: str) -> Iterator[bytes]:
+    def stream(self, message: str, user_id: UUID | None = None) -> Iterator[bytes]:
         """
         Streams a conversation with the assistant.
+        Add a user id to keep chat histories separate for different users.
+
+        Args:
+            message (str): The message to send.
+            user_id (str): An optional user id. Defaults to None.
         """
 
         for msg in self.api_client.get_stream(
-            f"assistant/{self.config.id}/chat/stream", params={"message": message}
+            f"assistant/{self.config.id}/chat/stream",
+            params={"message": message, "user_id": user_id},
         ):
             yield msg
