14218382e93424cec25e68f991652f8c882b11c5
Ion <iontapordei@gmail.com>
2024-09-24 12:08:02 +0000

    update example

--- examples/assistant/private_chats.py
+++ examples/assistant/private_chats.py
@@ -6,28 +6,28 @@ assistant = firedust.assistant.load("Sam")
 
 # all conversations are private to a given chat group and
 # persist in the assistant's memory
-user1 = "Pippin"
-user2 = "Merry"
+group1 = "group1"
+group2 = "group2"
 
 # user1 shares some important information which will be
 # stored in the assistant's memory
 assistant.chat.message(
     message="My favourite desert is cinnamon bun.",
-    username=user1,
-    chat_group="1",
+    username="Merry",
+    chat_group=group1,
 )
 response = assistant.chat.message(
     message="What is my favourite desert?",
-    username=user1,
-    chat_group="1",
+    username="Merry",
+    chat_group=group1,
 )
 assert "cinnamon bun" in response.content.lower()
 
 # user2 has no access to this information if they are not part of the chat group
 response = assistant.chat.message(
     message="What is Pippin's favourite desert?",
-    username=user2,
-    chat_group="2",
+    username="Pippin",
+    chat_group=group2,
 )
 assert "cinnamon bun" not in response.content.lower()
 
